37e31ccf186fa1703e1c15fae7f7c46b789123c4
Kate Simmons <kate.a.simmons@gmail.com>
2016-02-14 08:16:08 -0600

    first commit layout and outlets/actions in place

--- rating-sample/ViewController.swift
+++ rating-sample/ViewController.swift
@@ -9,6 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //outlets
+    @IBOutlet weak var segmentedControls: UISegmentedControl!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var timesWornLabel: UILabel!
+    @IBOutlet weak var lastWornLabel: UILabel!
+    @IBOutlet weak var favoriteLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +29,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //actions
+    @IBAction func wear(sender: AnyObject) {
+    }
+    
+    @IBAction func rate(sender: AnyObject) {
+    }
 
 }
 
